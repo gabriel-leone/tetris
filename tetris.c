8e985ed02070917cf5e2963815394d7340cf0b5a
Gabriel Leone <110470412+gabriel-leone@users.noreply.github.com>
2023-06-19 21:57:07 -0300

    corrigi o bug "double free or corruption (out)"

--- tetris.c
+++ tetris.c
@@ -33,10 +33,10 @@ const Shape ShapesArray[7]= {
 Shape CopyShape(Shape shape){
     Shape new_shape = shape;
     char **copyshape = shape.array;
-    new_shape.array = (char*)malloc(new_shape.width*sizeof(char));
+    new_shape.array = (char**)malloc(new_shape.width * sizeof(char*));
     int i, j;
     for(i = 0; i < new_shape.width; i++){
-        new_shape.array[i] = (char*)malloc(new_shape.width*sizeof(char));
+        new_shape.array[i] = (char*)malloc(new_shape.width * sizeof(char));
         for(j=0; j < new_shape.width; j++) {
             new_shape.array[i][j] = copyshape[i][j];
         }
